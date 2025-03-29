5a567931c1bab200456a6820480a3c38d34f2f50
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-03-29 14:53:15 +0800

    placingPieces

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -13,6 +13,31 @@ public class ChessBoard {
     }
 
     private void setupPieces(){
-        // Initial setup
+        // Place Rooks
+        board[0][0] = new Rook(PieceColor.BLACK, new Position(0, 0));
+        board[0][7] = new Rook(PieceColor.BLACK, new Position(0, 7));
+        board[7][0] = new Rook(PieceColor.WHITE, new Position(7, 0));
+        board[7][7] = new Rook(PieceColor.WHITE, new Position(7, 7));
+        // Place Knights
+        board[0][1] = new Knight(PieceColor.BLACK, new Position(0, 1));
+        board[0][6] = new Knight(PieceColor.BLACK, new Position(0, 6));
+        board[7][1] = new Knight(PieceColor.WHITE, new Position(7, 1));
+        board[7][6] = new Knight(PieceColor.WHITE, new Position(7, 6));
+        // Place Bishops
+        board[0][2] = new Bishop(PieceColor.BLACK, new Position(0, 2));
+        board[0][5] = new Bishop(PieceColor.BLACK, new Position(0, 5));
+        board[7][2] = new Bishop(PieceColor.WHITE, new Position(7, 2));
+        board[7][5] = new Bishop(PieceColor.WHITE, new Position(7, 5));
+        // Place Queens
+        board[0][3] = new Queen(PieceColor.BLACK, new Position(0, 3));
+        board[7][3] = new Queen(PieceColor.WHITE, new Position(7, 3));
+        // Place Kings
+        board[0][4] = new King(PieceColor.BLACK, new Position(0, 4));
+        board[7][4] = new King(PieceColor.WHITE, new Position(7, 4));
+        // Place Pawns
+        for (int i = 0; i < 8; i++) {
+            board[1][i] = new Pawn(PieceColor.BLACK, new Position(1, i));
+            board[6][i] = new Pawn(PieceColor.WHITE, new Position(6, i));
+        }
     }
 }
