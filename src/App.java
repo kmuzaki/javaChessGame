eecbcd8d68f72ac1ed5a04805e16fdda77aa9f3b
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-03-29 14:32:44 +0800

    init

---
+++ src/App.java
@@ -0,0 +1,11 @@
+/* Materials or references used:
+ hackr.io, How To Create A Java Chess Game Application for Beginners, https://hackr.io/blog/how-to-build-a-java-chess-game-app
+
+ */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
