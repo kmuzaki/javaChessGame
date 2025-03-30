21ba58f77209ba51547b4697b8bbc5fc7ddf10bd
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-03-30 12:24:36 +0800

    initPieceColor

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -40,4 +40,20 @@ public class ChessBoard {
             board[6][i] = new Pawn(PieceColor.WHITE, new Position(6, i));
         }
     }
+
+    public void movePiece(Position start, Position end) {
+        // Check if there is a piece at the start position and if the move is valid
+        if (board[start.getRow()][start.getColumn()] != null &&
+                board[start.getRow()][start.getColumn()].isValidMove(end, board)) {
+      
+            // Perform the move: place the piece at the end position
+            board[end.getRow()][end.getColumn()] = board[start.getRow()][start.getColumn()];
+      
+            // Update the piece's position
+            board[end.getRow()][end.getColumn()].setPosition(end);
+      
+            // Clear the start position
+            board[start.getRow()][start.getColumn()] = null;
+        }
+      }
 }
